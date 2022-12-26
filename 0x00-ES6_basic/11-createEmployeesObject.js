15ee7115d0fedeff979e6df35fdee734229beea8
abrhamadddis <abrhamaddis32@gmail.com>
2022-12-26 20:52:17 +0300

    iterable

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const output = {
-    departmentName: employees,
-  };
-  return output;
-}
+    return {
+      [departmentName]: employees,
+    };
+  }

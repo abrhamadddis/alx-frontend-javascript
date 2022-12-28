69e068362710f1e469228ac0fd99f91f0d3021cc
abrhamadddis <abrhamaddis32@gmail.com>
2022-12-28 11:38:56 +0300

    catch me if you can

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,8 @@
+export default function getFullResponseFromAPI(success) {
+  const outPut = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: '200', body: 'Success' });
+    } else reject(new Error('The fake API is not working  currently'));
+  });
+  return outPut;
+}

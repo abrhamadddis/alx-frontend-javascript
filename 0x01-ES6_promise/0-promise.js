93ed99c7e86a019b3abb8b54ba9fa921d07c0fd0
abrhamadddis <abrhamaddis32@gmail.com>
2022-12-28 11:08:26 +0300

    requirment fail

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,9 @@
 export default function getResponseFromAPI() {
-    const promise = new Promise((resolve, reject) => {
-      const error = false;
-      if (error) {
-        resolve('sucess');
-      } else reject(new Error('error happend'));
-    });
-    return promise;
-  }
+  const promise = new Promise((resolve, reject) => {
+    const error = false;
+    if (error) {
+      resolve('sucess');
+    } else reject(new Error('error happend'));
+  });
+  return promise;
+}

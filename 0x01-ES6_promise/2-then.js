0f9acf9e291bca2998c5c4a8976a26ad61622c08
abrhamadddis <abrhamaddis32@gmail.com>
2023-01-02 11:44:03 +0300

    catch me if you can

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  const outPut = new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'Success' });
-    } else reject(new Error());
-  });
-  return outPut;
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
-handleResponseFromAPI()
-  .then(() => ({ status: 200, body: 'success' }))
-  .catch(() => new Error())
-  .finally(() => console.log('Got a response from the API'));

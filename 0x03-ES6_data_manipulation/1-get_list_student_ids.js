32f170de49f7b8603e0f9485f106b5a45191f7c0
abrhamadddis <abrhamaddis32@gmail.com>
2023-01-03 13:07:50 +0300

    more mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,8 @@
 export default function getListStudentIds(arr) {
-    const result = Array.isArray(arr);
-    if (result){
-        return arr.map(element => element.id)
-    }
-    else {
-        return [];
-    }
+  const result = Array.isArray(arr);
+  if (result) {
+    return arr.map((element) => element.id);
+  }
+
+  return [];
 }
